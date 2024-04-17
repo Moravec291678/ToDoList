bf555030dd8020214a6b29573950a293ee504123
unknown <david.moravec2@seznam.cz>
2024-04-17 20:39:21 +0200

    no localstorage

--- main.js
+++ main.js
@@ -2,36 +2,64 @@ const main = document.querySelector(".main")
 const headerInput = document.querySelector("#headerInput");
 const addTaskBtn = document.querySelector("#addTask");
 const tasks = document.querySelector("#tasks");
-let inputValue = "";
-
-// do inputValue vztahne hodnotu z inputu
-headerInput.addEventListener("input", function (event) {
-  inputValue = event.target.value;
-});
 
 
+addTaskBtn.addEventListener("click", () => {
+  const task = headerInput.value;
+  if (!headerInput.value) {
+    alert("Nemáš vyplněné pole")
+    return
+  }
 
-//vytvori paragraf zapise do nej hodnotu z inputu a prida do `div #tasks`
-addTaskBtn.addEventListener("click", function(event){
-  let p = document.createElement("p");
-  p.classList.add("task");
-  const controlButtons = document.createElement("div");
-  const editBtn = document.createElement("button");
-  const removeBtn = document.createElement("button");
-  controlButtons.classList.add("controlButtons");
-  editBtn.classList.add("controlBtn","edit");
-  removeBtn.classList.add("controlBtn","remove");
-  p.textContent = ` - ${inputValue}`;
-  controlButtons.appendChild(editBtn);
-  controlButtons.appendChild(removeBtn);
-  p.appendChild(controlButtons);
-  
-  tasks.appendChild(p);
+  const taskElement = document.createElement('div');
+  taskElement.classList.add('task')
+  const taskContentElement = document.createElement('div');
+  taskContentElement.classList.add('content');
 
+  taskElement.appendChild(taskContentElement);
 
+  const taskInputElement = document.createElement('input');
+  taskInputElement.classList.add('text');
+  taskInputElement.type = 'text';
+  taskInputElement.value = task;
+  taskInputElement.setAttribute('readonly', 'readonly');
+
+  taskContentElement.appendChild(taskInputElement);
+
+  const taskActionElement = document.createElement('div');
+  taskActionElement.classList.add('actions');
+
+  const taskEditElement = document.createElement('button');
+  taskEditElement.classList.add('edit');
+  let index = 1;
+
+  const taskDeleteElement = document.createElement('button');
+  taskDeleteElement.classList.add('delete');
+
+  taskActionElement.appendChild(taskEditElement);
+  taskActionElement.appendChild(taskDeleteElement);
+
+  taskElement.appendChild(taskActionElement);
+  tasks.appendChild(taskElement);
 
   headerInput.value = "";
-});
+  taskEditElement.addEventListener('click', () =>{
+    if (index) {
+      taskInputElement.removeAttribute('readonly');
+      taskInputElement.focus();
+      taskEditElement.style.backgroundImage = "url('img/save.png')"
+      index = 0;
+      
+    }
+    else {
+      taskInputElement.setAttribute('readonly', 'readonly');
+      taskEditElement.style.backgroundImage = "url('img/edit.png')"
+      index = 1;
+    }
+  })
 
+  taskDeleteElement.addEventListener('click', () => {
+    tasks.removeChild(taskElement);
+  })
 
- 
+})
